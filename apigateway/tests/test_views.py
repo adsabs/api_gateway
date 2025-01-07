5303c975a5e5741279af6434e86c0c347aed8bcd
tjacovich <tjacovich@cfa.harvard.edu>
2025-01-06 20:20:12 -0500

    Fix valid password.

--- apigateway/tests/test_views.py
+++ apigateway/tests/test_views.py
@@ -28,7 +28,7 @@ class TestBootstrapView:
             user.active = True
             user.confirmed_at = datetime.utcnow()
             user.email = "test@gmail.com"
-            user.password = "valid_password"
+            user.password = "Valid_password1"
             user.fs_uniquifier = "test_user"
             session.add(user)
             session.commit()
@@ -81,7 +81,7 @@ class TestUserAuthView:
             user.active = True
             user.confirmed_at = datetime.utcnow()
             user.email = "test@gmail.com"
-            user.password = "valid_password"
+            user.password = "Valid_password1"
             user.fs_uniquifier = "unique_id"
             session.add(user)
             session.commit()
@@ -98,7 +98,7 @@ class TestUserAuthView:
             user.id = 123
             user.active = True
             user.email = "test_unverified@gmail.com"
-            user.password = "valid_password"
+            user.password = "Valid_password1"
             user.fs_uniquifier = "unique_id_unverified"
             app.db.session.add(user)
             app.db.session.commit()
@@ -125,14 +125,14 @@ class TestUserAuthView:
 
     def test_post_invalid_email(self, app, authenticated_user, user_auth_view):
         with app.test_request_context(
-            json={"email": "invalid@gmail.com", "password": "valid_password"}
+            json={"email": "invalid@gmail.com", "password": "Valid_password1"}
         ):
             with pytest.raises(Unauthorized):
                 user_auth_view.post()
 
     def test_post_unverified_account(self, app, unverified_user, user_auth_view):
         with app.test_request_context(
-            json={"email": unverified_user.email, "password": "valid_password"}
+            json={"email": unverified_user.email, "password": "Valid_password1"}
         ):
             with pytest.raises(Unauthorized):
                 user_auth_view.post()
@@ -237,7 +237,7 @@ class TestUserManagementView:
             user.active = True
             user.confirmed_at = datetime.utcnow()
             user.email = "test@gmail.com"
-            user.password = "valid_password"
+            user.password = "Valid_password1"
             user.fs_uniquifier = "unique_id"
             session.add(user)
             session.commit()
@@ -293,7 +293,7 @@ class TestLogoutView:
             user.active = True
             user.confirmed_at = datetime.utcnow()
             user.email = "test@gmail.com"
-            user.password = "valid_password"
+            user.password = "Valid_password1"
             user.fs_uniquifier = "unique_id"
             session.add(user)
             session.commit()
@@ -478,7 +478,7 @@ class TestVerifyEmailView:
             user.active = True
             user.confirmed_at = datetime.utcnow()
             user.email = "test@gmail.com"
-            user.password = "valid_password"
+            user.password = "Valid_password1"
             user.fs_uniquifier = "unique_id"
             session.add(user)
             session.commit()
